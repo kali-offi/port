107f3a8adc314d589bdc35ec38f9c536f5b0c2aa
Your Name <you@example.com>
2025-02-13 09:28:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navy from './Navbar/Navy.jsx'
+import Header from './Header/Header.jsx'
+import About from './About/About.jsx'
+import './App.css'
+import Port from './portfolio/portfolio.jsx'
+import Contact from './Contact/Contact.jsx'
+import Footer from './Footer/Footer.jsx';
+
+
+function App() {
+
+  return (
+    <>
+      <Navy />
+      <Header />
+      <About />
+      <Port />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
